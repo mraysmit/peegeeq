149d7e6309ec043dc4eddf3edd76a84659a86792
mraysmit <raysmith.subs@gmail.com>
2026-07-14 19:13:29 +0800

    refactor: Update Spring Boot integration examples and dependencies

--- peegeeq-examples-spring/src/test/java/dev/mars/peegeeq/examples/springbootpriority/SpringBootPriorityApplicationTest.java
+++ peegeeq-examples-spring/src/test/java/dev/mars/peegeeq/examples/springbootpriority/SpringBootPriorityApplicationTest.java
@@ -38,7 +38,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
